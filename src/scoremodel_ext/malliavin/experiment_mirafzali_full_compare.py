293647598f1f116e96e03a268d4a4ce0712b1bed
morimoto0606 <yuusuke.morimoto@gmail.com>
2026-06-03 08:21:45 +0900

    a add approximation results comparison

--- src/scoremodel_ext/malliavin/experiment_mirafzali_full_compare.py
+++ src/scoremodel_ext/malliavin/experiment_mirafzali_full_compare.py
@@ -21,13 +21,29 @@ run_residual_multiseed_eval(
     seeds=[0],
     configs=[
         {
-            "_key": "full_big_forward_init",
+            "_key": "approx_lowt_stationary",
+            "method": "mirafzali",
+            "correction": "approx",
+            "n_steps_rev": 1000,
+        },
+        {
+            "_key": "a_correction_lowt_stationary",
+            "method": "mirafzali",
+            "correction": "a_correction",
+            "n_steps_rev": 1000,
+        },
+        {
+            "_key": "full_lowt_stationary",
             "method": "mirafzali",
             "correction": "mirafzali_full",
             "n_steps_rev": 1000,
         },
     ],
-    times=[0.005, 0.01, 0.02, 0.035, 0.05, 0.075, 0.10, 0.20, 0.35, 0.50, 0.75, 1.00,],
+    times=[
+        0.005, 0.01, 0.02, 0.035,
+        0.05, 0.075, 0.10,
+        0.20, 0.35, 0.50, 0.75, 1.00,
+    ],
     reverse_init="stationary",
     hidden=2048,
     n_blocks=6,
@@ -36,5 +52,5 @@ run_residual_multiseed_eval(
     batch_size=4096,
     lr=2e-4,
     n_steps_per_unit=250,
-    outbase="results/mirafzali_full_swissroll_big_stationary_lowt_rev1000_1seed"
+    outbase="results/mirafzali_approx_vs_full_swissroll_lowt_stationary_1seed",
 )
